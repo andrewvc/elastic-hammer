4cf62d64e1ef23add9351b1effb9176f2f003b71
Andrew Cholakian <andrew@andrewvc.com>
2013-06-30 19:39:34 -0700

    Localstorage + max collection size

--- main.js
+++ main.js
@@ -5,12 +5,26 @@ Hammer = {
 Hammer.Request = Backbone.Model.extend({
   defaults: function () {
     return {
+      started: null,
+      ended: null,
       state: 'fresh', 
       method: 'GET',
       server: 'http://localhost:9200',
       path: '',
-      body: null
+      body: null,
+      response: null,
+      status: null
     };
+  },
+  url: function () {
+    return this.get('method') + '/' + this.get('path');
+  },
+  // Return statuses rounded down to the nearest 100. i.e. 201 -> 200
+  statusGroup: function () {
+    if (this.get('state') === 'errored' && !this.get('status')) return 'errored';
+    if (!this.get('status') === null) return null;
+    var s = this.get('status');
+    return s - (s % 100);
   },
   exec: function () {
     if (this.get('state') !== 'fresh') {
@@ -26,6 +40,9 @@ Hammer.Request = Backbone.Model.extend({
     if (m === 'POST' || m === 'PUT') {
       body = this.get('body');
     }
+    
+    this.set('started', Date.now());
+    
     $.ajax(
       this.url(),
       {
@@ -44,8 +61,10 @@ Hammer.Request = Backbone.Model.extend({
         self.set({state: 'errored', response: xhr.responseText, status: xhr.status});
         self.trigger('complete');
         self.trigger('error');
-      }
-    );
+    }).done(
+      function (xhr) {
+        self.set('ended', Date.now());
+    });
     
   },
   execRecord: function () {
@@ -59,26 +78,56 @@ Hammer.Request = Backbone.Model.extend({
 });
 
 Hammer.RequestHistory = Backbone.Collection.extend({
+  localStorage: new Backbone.LocalStorage("request-history"),
+  maxRecords: 500,
   model: Hammer.Request,
+  initialize: function () {
+    this.fetch();
+    this.on('add', this.trim, this);
+  },
+  trim: function () {
+    if (this.size() > this.maxRecords) {
+      var i = this.maxRecords;
+      var m;
+      while (m = this.models[i]) {
+        i++;
+        m.destroy();
+      }
+    }
+  },
   watch: function (request) {
-    var adder = function () {
+    var adder = null;
+    adder = function () {
       this.add(request);
+      request.save();
+      request.off('complete', adder);
     };
     request.on('complete', adder, this);
+  },
+  comparator: function (request) {
+    return(- request.get('started'));
   }
 });
 
-Hammer.CurrentRequestVM = function (request) {
+Hammer.RequestBaseVM = function (request) {
   _.extend(this, kb.viewModel(request));
-  
   this.exec = function (self) {
     request.execRecord();
   };
-
+  
+  var self = this;
   this.bodyCapable = ko.computed(function () {
     var m = this.method();
     return (m === 'POST' || m === 'PUT')
   }, this);
+
+  this.url = ko.computed(function (self) {
+    return request.url();
+  });
+};
+
+Hammer.CurrentRequestVM = function (request) {
+  _.extend(this, new Hammer.RequestBaseVM(request));
 
   this.checkCtrlEnter = function (self,e) {
     if (e.keyCode == 13 && e.ctrlKey) {
@@ -87,150 +136,35 @@ Hammer.CurrentRequestVM = function (request) {
     }
     return true
   }
+};
+
+Hammer.HistoricalRequestVM = function (request) {
+  _.extend(this, new Hammer.RequestBaseVM(request));
+
+  this.responseFmt = ko.computed(function() {
+    return JSON.stringify(this.response());
+  }, this);
+
+  this.statusGroup = ko.computed(function () {
+    this.status.peek();
+    this.state.peek();
+    return request.statusGroup();
+  }, this);
+};
+
+Hammer.HistoricalRequestsVM = function (requests) {
+  this.requests = kb.collectionObservable(requests, {view_model: Hammer.HistoricalRequestVM})
 }
 
 $(function () {
   Hammer.Data.history = new Hammer.RequestHistory();
   Hammer.Data.current = new Hammer.Request();
+  
+  // The Current Request
   var curReqVM = new Hammer.CurrentRequestVM(Hammer.Data.current);
   ko.applyBindings(curReqVM, $('#current-request')[0]);
   
+  // The list of past requests
+  var histReqsVM = new Hammer.HistoricalRequestsVM(Hammer.Data.history);
+  ko.applyBindings(histReqsVM, $('#request-history')[0]);
 });
-
-/*
-
-
-  Hammer.RequestFormView = Backbone.View.extend({
-  className: 'request-form',
-  events: {
-  keypress: 'keypress',
-  submit: 'blockSubmit',
-  change: 'change',
-  "click input[type=submit]": "exec"
-  },
-  initialize: function () {
-  this.tmpl = Hammer.templates['request-form-tmpl'];
-  this.render();
-  this.on('change:model',
-  function (newModel) {
-  this.model = newModel;
-  },
-  this);
-  },
-  render: function () {
-  this.$el.html(this.tmpl(this.model.toJSON()));        
-  },
-  change: function () {
-  this.readForm();
-  var $f = this.$el.find('form');
-
-  $f.attr('class', 'method-' + this.model.get('method').toLowerCase());
-  },
-  updateForm: function () {
-  var $f = this.$el.find('form');
-
-  $f.find('#ajax-method').val(this.model.get('method'));
-  $f.find('#server').val(this.model.get('server'));
-  $f.find('#path').val(this.model.get('path'));
-  $f.find('textarea[name=body]').val(this.model.get('body'));
-  },
-  readForm: function () {
-  var $f = this.$el.find('form');
-  var attrs = {
-  method: $f.find('#ajax-method').val(),
-  server: $f.find('#server').val(),
-  path: $f.find('#path').val(),
-  body: $f.find('textarea[name=body]').val()
-  };        
-  this.model.set(attrs);
-  },
-  exec: function () {
-  this.readForm();
-  this.model.exec();
-  },
-  blockSubmit: function (e) {
-  e.preventDefault();  
-  },
-  keypress: function (e) {
-  if ($(e.target).attr('name') === 'body') {
-  if (Hammer.Util.isCtrlEnter(e)) {
-  e.preventDefault();
-  this.exec();
-  }
-  } else if (e.keyCode === 13) {
-  e.preventDefault();
-  this.exec();
-  }
-  }
-  });
-
-
-
-  Hammer.RequestView = Backbone.View.extend({
-  className: 'request',
-  initialize: function () {
-  this.tmpl = Hammer.templates['request-tmpl'];
-  },
-  render: function () {
-  this.$el.html(this.tmpl(this.model.toJSON()));
-  },
-  adjustSize: function () {
-  this.$el.find('textarea').each(function (i,ta) {
-  console.log(ta,i,ta.scrollHeight);
-  var rows = Math.ceil(ta.scrollHeight / 13);
-  $(ta).attr('rows', (rows > 20 ? 20 : rows));
-  });
-  }
-  });
-
-  Hammer.RequestHistoryView = Backbone.View.extend({
-  tagName: 'section',
-  className: 'request-history',
-  initialize: function (opts) {
-  this.tmpl = Hammer.templates['request-history-tmpl'];
-  this.history = opts.history;
-  this.render();
-  
-  this.history.on('add', this.addRequest, this);
-  },
-  render: function () {
-  this.$el.html(this.tmpl({content: ""}));
-  },
-  addRequest: function (request) {
-  var requestView = new Hammer.RequestView({model: request});
-  requestView.render();
-  $('.requests',this.$el).prepend(requestView.el);
-  requestView.adjustSize();
-  }
-  });
-
-  Hammer.RequestRouter = Backbone.Router.extend({
-  initialize: function () {
-  this.history = new Hammer.RequestHistory;
-  this.reqHistoryView = new Hammer.RequestHistoryView({history: this.history});
-  
-  this.curReqView = new Hammer.RequestFormView({model: new Hammer.Request});
-
-  this.showNew();
-  // why does this not work?
-  this.curReqView.$el.find('#path').focus();
-  
-  $('#content').append(this.curReqView.el);
-  $('#content').append(this.reqHistoryView.el);
-  },
-  showNew: function () {
-  this.curReq = this.curReq ? this.curReq.clone() : new Hammer.Request;
-  this.history.watch(this.curReq);
-  this.curReq.set('state', 'fresh');
-  this.curReqView.trigger('change:model', this.curReq);
-  this.curReq.on('complete', this.showNew, this);
-  }
-  });
-
-  $(function () {
-  Hammer.loadTemplates();
-  window.hammerRouter = new Hammer.RequestRouter;
-  Backbone.history.start();
-  });
-
-*/
